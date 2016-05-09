80947bca0ad50d9ff1bced8f3092f7b5f9601047
Jan Tumanov <jan.tumanov@gmail.com>
2016-05-09 16:10:29 +0300

    New tasks

---
+++ src/com/javarush/test/level26/lesson10/home02/Producer.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level26.lesson10.home02;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by jan on 5/9/16.
+ */
+public class Producer implements Runnable
+{
+    protected ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            int i = 1;
+            while (true) {
+
+                map.put(String.valueOf(i), "Some text for " + i++);
+
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+    }
+}
